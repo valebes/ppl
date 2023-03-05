689df93a31fcc912bff11c084e222a0fad249584
Valerio Besozzi <valebes@gmail.com>
2023-03-05 13:35:40 +0100

    fix

--- examples/image-processing/pspp_map.rs
+++ examples/image-processing/pspp_map.rs
@@ -69,7 +69,7 @@ pub fn pspp_map(dir_name: &str, threads: usize) {
 
     let mut p = parallel![
         Source {
-            window: threads,
+            window: threads * 4,
             all_images: all_images,
         },
         Map::new(threads, |mut image: Image| -> Image {
