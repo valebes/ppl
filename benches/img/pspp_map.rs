a228647d970880a6f3ee2daa454c91e28202df7e
Valerio Besozzi <valebes@gmail.com>
2023-03-06 17:50:27 +0100

    Change pspp-map bench

--- benches/img/pspp_map.rs
+++ benches/img/pspp_map.rs
@@ -3,24 +3,12 @@ use raster::filter;
 use raster::Image;
 
 pub fn pspp_map(images: Vec<Image>, threads: usize) {
-    let mut pool = ThreadPool::new(threads * 5, true);
-    let a = pool.par_map(images, |mut image: Image| -> Image {
+    let mut pool = ThreadPool::new(threads, true);
+    let _a = pool.par_map(images, |mut image: Image| -> Image {
         filter::saturation(&mut image, 0.2).unwrap();
-        image
-    });
-    let b = pool.par_map(a, |mut image: Image| -> Image {
         filter::emboss(&mut image).unwrap();
-        image
-    });
-    let c = pool.par_map(b, |mut image: Image| -> Image {
         filter::gamma(&mut image, 2.0).unwrap();
-        image
-    });
-    let d = pool.par_map(c, |mut image: Image| {
         filter::sharpen(&mut image).unwrap();
-        image
-    });
-    let _ = pool.par_map(d, |mut image: Image| {
         filter::grayscale(&mut image).unwrap();
         image
     });
