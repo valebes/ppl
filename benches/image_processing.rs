18e98ff1eb7027811a2a63936913d050ff52ac8a
Valerio Besozzi <valebes@gmail.com>
2023-05-14 14:52:14 +0200

    Incremented sample size in behc

--- benches/image_processing.rs
+++ benches/image_processing.rs
@@ -9,7 +9,7 @@ fn image_processing(criterion: &mut Criterion) {
     group
         .sampling_mode(SamplingMode::Auto)
         .plot_config(plot_cfg)
-        .sample_size(10);
+        .sample_size(20);
 
     // Parses the images.
     let images = std::fs::read_dir("benches/img/images")
